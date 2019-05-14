78a8164cd7b993a2a036cad3e818f40ed86961ca
Shesky17 <aidenx58@gmail.com>
2019-05-13 23:03:59 -0400

    Updated

--- dns_update_script.sh
+++
@@ -1,2 +0,0 @@
-wget https://8bOc94Q0cTdEJCsJ:TVIgYiRPaB0JOp9g@domains.google.com/nic/update?hostname=www.shesky17.com - O dns_update_result.txt
-echo " Last run: `date`" >> ./dns_updates/dns_update_result.txt
