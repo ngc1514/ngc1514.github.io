a70a9d7c43a22d765d70e964cfc2b3d041f879b2
Shesky17 <aidenx58@gmail.com>
2019-06-25 03:05:15 -0400

    Bug fixed

--- static/js/displayTimeMessage.js
+++ static/js/displayTimeMessage.js
@@ -3,19 +3,19 @@ function displayMessage()
     var today1 = new Date();
     var hour = today1.getHours();
 
-    if (hour >= 23 && hour <= 4)
+    if (hour >= 23 && hour <= 5)
     {
-       var msg = 'It&#39;s late. ';
+       var msg = 'yOu sHoUld gO To bEd. ';
        document.getElementById("msg").innerHTML = msg;
     }
     if (hour > 5 && hour < 12)
     {
-    var msg = 'Good Morning. ';
-    document.getElementById("msg").innerHTML = msg;
+        var msg = 'Good Morning. ';
+        document.getElementById("msg").innerHTML = msg;
     }	
     if (hour >= 12 && hour <= 20)
     {
-        var msg = 'Work Blyat';
+        var msg = 'Go to work.. ';
         document.getElementById("msg").innerHTML = msg;
     }   	
 }
