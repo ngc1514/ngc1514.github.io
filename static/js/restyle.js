7b7849257bd720a751be232f54f170f19087a8bb
Shesky17 <aidenx58@gmail.com>
2019-07-03 21:00:59 -0400

    Bug fixed

--- static/js/restyle.js
+++ static/js/restyle.js
@@ -78,4 +78,18 @@ function restyle(strStyle)
 			headers[i].style.color = "#ffffff";
 		}
 	}
+	else if(strStyle === 'matterhorn')
+	{
+		document.body.style.background = "url(https://www.shesky17.com/static/bkg_img/matterhorn.jpg)";
+		document.body.style.backgroundSize = "cover";
+		document.getElementById('credit').innerHTML = "<center><font size='4' color='white'>Background credit to: <br> Leung Jonathan. 'Amazing Summer Night.' <i>Flickr</i>, 24 Aug. 2014, https://www.flickr.com/photos/jonathan-leung/15425904419/.</font></center>";
+		for(var i=0; i<font.length; i++)
+		{
+			font[i].style.color = "#ffffff";
+		}
+		for(var i=0; i<headers.length; i++)
+		{
+			headers[i].style.color = "#ffffff";
+		}
+	}
 }
