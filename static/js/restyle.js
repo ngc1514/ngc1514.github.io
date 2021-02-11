a17c27c5226b29adea59682ba6b13cf46b6edb70
Shesky17 <aidenx58@gmail.com>
2021-02-11 00:55:21 -0500

    Updated shitty site

--- static/js/restyle.js
+++ static/js/restyle.js
@@ -4,21 +4,8 @@ function restyle(strStyle)
 	var headers = document.getElementsByTagName('h1');
 	var uls = document.getElementsByTagName('li');
 
-	if(strStyle === 'white')
-	{
-		document.body.style.background = "url(../static/bkg_img/whiteIndex.png)";
-		document.body.style.backgroundSize = "cover";
-		document.getElementById('credit').innerHTML = '';
-		for(var i=0; i<font.length; i++)
-		{
-			font[i].style.color = "#000000";
-		}
-		for(var i=0; i<headers.length; i++)
-		{
-			headers[i].style.color = "#000000";
-		}
-	}
-	else if(strStyle === 'dark')
+
+	if(strStyle === 'dark')
 	{
 		document.body.style.background = "#141d26";
 		document.getElementById('credit').innerHTML = '';
@@ -35,20 +22,7 @@ function restyle(strStyle)
 			uls[i].style.color = "#1b2737";
 		}
 	}
-	else if(strStyle === 'gray')
-	{
-		document.body.style.background = "url(../static/bkg_img/grayIndex.png)";
-		document.body.style.backgroundSize = "cover";
-		document.getElementById('credit').innerHTML = '';for (var i=0; i<font.length; i++);
-		for(var i=0; i<font.length; i++)
-		{
-			font[i].style.color = "#000000";
-		}
-		for(var i=0; i<headers.length; i++)
-		{
-			headers[i].style.color = "#000000";
-		}
-	}
+
 	else if(strStyle === 'stars')
 		{
 			document.body.style.background = "url(../static/bkg_img/stars.jpg) no-repeat center center fixed";
@@ -61,35 +35,6 @@ function restyle(strStyle)
 			}
 			for(var i=0; i<headers.length; i++)
 			{
-			headers[i].style.color = "#ffffff";
-		}
-	}
-	else if(strStyle === 'matterhorn')
-	{
-		document.body.style.background = "url(../static/bkg_img/matterhorn.jpg) no-repeat center center fixed";
-		document.body.style.backgroundSize = "cover";
-
-		document.getElementById('credit').innerHTML = '';
-		for(var i=0; i<font.length; i++)
-		{
-			font[i].style.color = "#ffffff";
-		}
-		for(var i=0; i<headers.length; i++)
-		{
-			headers[i].style.color = "#ffffff";
-		}
-	}
-	else if(strStyle === 'night')
-	{
-		document.body.style.background = "url(../static/bkg_img/night.jpg)";
-		document.body.style.backgroundSize = "cover";
-		document.getElementById('credit').innerHTML = "<center><font size='4' color='white'>Background credit to: <br> Leung Jonathan. 'Amazing Summer Night.' <i>Flickr</i>, 24 Aug. 2014, https://www.flickr.com/photos/jonathan-leung/15425904419/.</font></center>";
-		for(var i=0; i<font.length; i++)
-		{
-			font[i].style.color = "#ffffff";
-		}
-		for(var i=0; i<headers.length; i++)
-		{
 			headers[i].style.color = "#ffffff";
 		}
 	}
